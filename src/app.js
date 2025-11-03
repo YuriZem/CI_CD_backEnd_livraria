ee1528d4547a8f2dfc4b3d0c1fa9b7ccbda9011b
YuriZem <yurizem333@gmail.com>
2025-11-03 10:39:17 -0300

    testando CD

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import dbConnect from './config/dbConnect.js';
 import livro from './models/Livro.js';
 import e from 'express';
 
-//teste
+//teste 2
 const conect = await dbConnect();
 
 conect.on("error", (error) => {
